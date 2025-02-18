8eddcfe435346891d29b3f84b6131e713733fe7d
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2025-02-18 15:46:38 +0530

    student crud route add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// students routes
+Route::get('/', [StudentController::class, 'index'])->name('std_home');
+Route::get('stdCreate', [StudentController::class, 'create'])->name('std_create');
+Route::get('stdEdit/{id}', [StudentController::class, 'edit'])->name('std_edit');
+//student data routes
+Route::post('stdStore', [StudentController::class, 'store'])->name('std_store');
+Route::put('stdUpdate/{id}', [StudentController::class, 'update'])->name('std_update');
+Route::delete('stdDelete/{id}', [StudentController::class, 'destroy'])->name('std_delete');
+
